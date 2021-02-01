e41aae75f01b2c8794412534943f8b6a18df1db9
Kareem52708 <75049224+Kareem52708@users.noreply.github.com>
2021-02-01 09:24:22 -0500

    Add files via upload

---
+++ Class35/sketch.js
@@ -0,0 +1,47 @@
+var ball;
+var dbball;
+var position;
+var database;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+ 
+    dbball = database.ref('ball/position');
+    dbball.on("value",readPosition);
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+       writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        x:position.x+x,
+        y:position.y+y,
+    });   
+}
+
+
+function readPosition(data){
+position = data.val();//storing the db x & y values to position
+ball.x = position.x;
+ball.y = position.y;
+}
